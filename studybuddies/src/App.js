f2063cc529cc3b0a9b219604c27589853387f68c
Victoria-Williamson <victoriaw@Victorias-MacBook-Pro.local>
2020-09-25 23:57:48 -0400

    working on home page

--- studybuddies/src/App.js
+++ studybuddies/src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Breadcrumb} from 'react-bootstrap/Breadcrumb';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="tabBar">
+        <div id="websiteSymbol"> *insert symbol* </div>
+        <div className="tabButton" id="tab1" href="#"> Tab 1</div>
+        <div className="tabButton" id="tab2" href="#"> Tab 2</div>
+        <div className="tabButton" id="tab3" href="#"> Tab 3</div>
+        <div className="tabButton" id="tab4" href="#"> Tab 4</div>
+        <div className="tabButton" id="tab5" href="#"> Tab 5</div>
+      </div>
+      <div className="auth" id="authentication">
+        <div id="toggleLogin">
+          Log In
+        </div>
+        <div className="auth" id="signUp">
+          Sign Up
+        </div>
+      </div>
+      Nothing Here yet, but this can be the main page 
     </div>
   );
 }
