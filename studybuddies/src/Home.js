95e3ac25ba5ed7091d653053d63d0efbda843086
weiiyii <cwy.sophiechen@knights.ucf.edu>
2020-09-26 22:16:21 -0400

    home page update
    css file not done yet

---
+++ studybuddies/src/Home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import "./Home.css";
+import divider from "./img/divider-line.png";
+import titleBar from "./title-bar.png";
+
+function Home {}
+{
+    return (
+        <div>
+            Home
+            <div id="titleBar">
+            <img id="titleBar" src= {titleBar}></img>
+            </div>
+            <div id="miroodles-homepage"/>
+            <div id="heythere-text"/>
+            <div id="how_lomg_to_study"/>
+            <div id="study_time_bar"/>
+            <div id="major"/>
+            <div id="subject"/>
+            <div id="Start_studying_button"/>
+            <div id="book-party"/>
+            <div id="avatar1"/>
+            <div id="change_arrow"/>
+            <div id="home_grey_username"/>
+            <div id="change"/>
+            <div id="home_right_arrow"/>
+            <div id="home_email_address"/>
+            <div id="something@example.com"/>
+            <div id="change"/>
+            <div id="home_right_arrow"/>
+            <div id="home_Rectangle"/>
+            <div id="display_email"/>
+            <div id="chat_status"/>
+            <div id="chatstatus_text"/>
+            <div id="changepassword"/>
+            <div id="home_right_arrow"/>
+            <div id="horizontal_line"/>
+            <div id="mostrecentbuddy"/>
+            <div id="avatar1"/>
+            <div id="home_black_username"/>
+            <div id="view_profile"/>
+            <div id="avatar2"/>
+            <div id="home_black_username"/>
+            <div id="view_profile"/>
+            <div id="avatar3"/>
+            <div id="home_black_username"/>
+            <div id="view_profile"/>
+            <div id="logout"/>
+        </div> 
+    )
+}
